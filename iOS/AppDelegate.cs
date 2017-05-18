76a03f6f0d108852e46c4e8aaceff20c5d625357
jers007 <jers007@gmail.com>
2017-05-18 18:59:07 -0500

    Updates
    Just updates of Visual Studio

--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -7,16 +7,17 @@ using UIKit;
 
 namespace ZwabyBeta.iOS
 {
-	[Register("AppDelegate")]
-	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
-	{
-		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
-		{
-			global::Xamarin.Forms.Forms.Init();
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            Microsoft.WindowsAzure.MobileServices.CurrentPlatform.Init();
+            global::Xamarin.Forms.Forms.Init();
 
-			LoadApplication(new App());
+            LoadApplication(new App());
 
-			return base.FinishedLaunching(app, options);
-		}
-	}
+            return base.FinishedLaunching(app, options);
+        }
+    }
 }
